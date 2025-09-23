6432f66ad3fa7dea1d5dc254bd749b606f706539
Richard Croft <tk@terrifickid.net>
2025-09-23 08:12:35 +0000

    update

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,9 @@
 import adapter from "@sveltejs/adapter-vercel";
+import dotenvJSON from "dotenv-json";
+// Load .env.json only if not in Vercel environment
+if (!process.env.VERCEL) {
+  dotenvJSON(); // Loads ./env.json by default
+}
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
