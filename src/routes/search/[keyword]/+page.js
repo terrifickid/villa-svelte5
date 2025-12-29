8be1e8c7647762bbeb1edeaa97bdadeb93f7c30c
Richard Croft <tk@terrifickid.net>
2025-12-29 09:04:02 +0000

    fixes

--- src/routes/search/[keyword]/+page.js
+++ src/routes/search/[keyword]/+page.js
@@ -22,7 +22,7 @@ export async function load({ params }) {
   const queryParams = {
     content_type: "property",
     query: keyword, // full-text search
-    limit: 100,
+    limit: 20,
     // 'fields.tags[in]': 'iCal',    // uncomment if you had this filter before
   };
 
