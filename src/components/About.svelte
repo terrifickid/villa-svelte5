1516aee2a5536aa67720657e53524568d8ce0b09
Richard Croft <tk@terrifickid.net>
2026-05-06 12:01:17 +0000

    text change

--- src/components/About.svelte
+++ src/components/About.svelte
@@ -1,12 +1,31 @@
 <section>
   <div class=" py-10">
-    <div>
+    <div class="grid grid-cols-2">
       <p class="mt-16 text-2xl font-normal">
         Founded in 2011 by Barbadian entrepreneur Lily Dash, Villabound evolved
         from her acclaimed Caribbean villa company, Caribound. What began as a
         passion for premium Barbados escapes has grown into a globally curated
         collection of the finest private villas, handpicked for discerning
         travelers who seek more than just a place to stay.
+      </p>
+      <p class="mt-16 text-2xl font-normal">
+        Villabound was founded in 2011 by Barbadian entrepreneur Lily Dash. Born
+        from her acclaimed Caribbean villa company Caribound, Villabound has
+        evolved from curating premium Barbados escapes into a globally respected
+        collection of the world’s finest private villas. Handpicked for
+        discerning travelers who seek far more than a place to stay, every
+        property is an exclusive masterpiece—complete with high-end amenities,
+        dedicated staff, priceless art, exquisite furnishings, and unforgettable
+        features like private gyms and personal spas.
+      </p>
+
+      <p class="mt-16 text-2xl font-normal">
+        We deliver a seamless, confidential experience tailored to the schedules
+        and lifestyles of our high-profile clientele. With a growing
+        international portfolio of livable works of art and our signature
+        Atelier Service, clients receive a dedicated Villa Specialist who crafts
+        bespoke journeys from start to finish. For more information on this
+        private service, call +1 (246) 424-5075.
       </p>
     </div>
   </div>
