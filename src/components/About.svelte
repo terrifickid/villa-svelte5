649e7bb1cb9d43fee01b0b29eff1376d3d9b763f
Richard Croft <tk@terrifickid.net>
2026-05-07 08:16:23 +0000

    update

--- src/components/About.svelte
+++ src/components/About.svelte
@@ -1,25 +1,24 @@
 <section>
-  <div class=" py-10">
-    <div class="grid grid-cols-2">
-      <p class="mt-16 text-2xl font-normal">
-        Founded in 2011 by Barbadian entrepreneur Lily Dash, Villabound evolved
-        from her acclaimed Caribbean villa company, Caribound. What began as a
-        passion for premium Barbados escapes has grown into a globally curated
-        collection of the finest private villas, handpicked for discerning
-        travelers who seek more than just a place to stay.
+  <div class=" border-t mt-12 py-10">
+    <div class="grid grid-cols-3 gap-6">
+      <p class="text-2xl font-normal">
+        Villabound was established in 2011 by a dynamic Barbadian entrepreneur
+        Lily Dash. The company grew out of her initial company Caribound; a
+        specialized service focused on Caribbean villa rentals.
       </p>
-      <p class="mt-16 text-2xl font-normal">
-        Villabound was founded in 2011 by Barbadian entrepreneur Lily Dash. Born
-        from her acclaimed Caribbean villa company Caribound, Villabound has
-        evolved from curating premium Barbados escapes into a globally respected
-        collection of the world’s finest private villas. Handpicked for
-        discerning travelers who seek far more than a place to stay, every
-        property is an exclusive masterpiece—complete with high-end amenities,
-        dedicated staff, priceless art, exquisite furnishings, and unforgettable
-        features like private gyms and personal spas.
+      <p class="ext-2xl font-normal">
+        In a few short years Lily took her experiences of villa rentals in the
+        premium market of Barbados and applied them to markets across the globe.
+        Villabound was designed to cater to the needs of a high-end, discerning
+        and discreet clientele. As a result our service involves the sourcing of
+        unique and exclusive villas around the world, meeting the demands of our
+        clients schedules. Every villa is carefully cultivated and represents
+        the finest selection each country has to offer. Boasting details such as
+        high-end amenities, inclusion of staff, priceless art pieces, fantastic
+        furnishings as well as unimaginable features from gyms to personal spas.
       </p>
 
-      <p class="mt-16 text-2xl font-normal">
+      <p class="font-normal">
         We deliver a seamless, confidential experience tailored to the schedules
         and lifestyles of our high-profile clientele. With a growing
         international portfolio of livable works of art and our signature
